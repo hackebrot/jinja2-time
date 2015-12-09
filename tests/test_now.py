b60b03c5f9ff8b203c309714c06922c2680a244e
Raphael Pierzina <raphael@hackebrot.de>
2015-12-09 23:47:03 +0100

    Implement a test for the extensions default datetime format

--- tests/test_now.py
+++ tests/test_now.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from freezegun import freeze_time
+from jinja2 import Environment, exceptions
 import pytest
-
-from jinja2 import Environment
 
 
 @pytest.fixture(scope='session')
@@ -10,5 +10,13 @@ def environment():
     return Environment(extensions=['jinja2_time.TimeExtension'])
 
 
-def test_foobar(environment):
-    assert environment
+def test_tz_is_required(environment):
+    with pytest.raises(exceptions.TemplateSyntaxError):
+        environment.from_string('{% now %}')
+
+
+@freeze_time("2015-12-09 23:33:01")
+def test_default_datetime_format(environment):
+    template = environment.from_string("{% now 'utc' %}")
+
+    assert template.render() == "2015-12-09"
