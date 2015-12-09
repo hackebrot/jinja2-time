1d7cd9fd4bd52cc5917373ff543c5cdb2b22e9bb
Raphael Pierzina <raphael@hackebrot.de>
2015-12-09 23:57:18 +0100

    Add a parametrized test for valid timezones

--- tests/test_now.py
+++ tests/test_now.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from freezegun import freeze_time
 from jinja2 import Environment, exceptions
-import pytest
 
 
 @pytest.fixture(scope='session')
@@ -16,7 +17,19 @@ def test_tz_is_required(environment):
 
 
 @freeze_time("2015-12-09 23:33:01")
-def test_default_datetime_format(environment):
+def test_utc_default_datetime_format(environment):
     template = environment.from_string("{% now 'utc' %}")
 
     assert template.render() == "2015-12-09"
+
+
+@pytest.fixture(params=['utc', 'local', 'Europe/Berlin'])
+def valid_tz(request):
+    return request.param
+
+
+@freeze_time("2015-12-09 23:33:01")
+def test_accept_valid_timezones(environment, valid_tz):
+    template = environment.from_string("{% now '" + valid_tz + "' %}")
+
+    assert '2015' in template.render()
