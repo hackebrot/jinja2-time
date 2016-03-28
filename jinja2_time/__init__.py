d0b56873b40acd3932f450293c363b01b5a709b9
Raphael Pierzina <raphael@hackebrot.de>
2016-03-28 18:47:03 +0100

    Fix flake8 issue in init

--- jinja2_time/__init__.py
+++ jinja2_time/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from .jinja2_time import TimeExtension
+
 __author__ = 'Raphael Pierzina'
 __email__ = 'raphael@hackebrot.de'
 __version__ = '0.1.0'
 
-from .jinja2_time import TimeExtension
 
 __all__ = ['TimeExtension']
